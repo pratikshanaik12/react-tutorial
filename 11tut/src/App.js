32d0241fe834cf29345e4a209e734f9f693ba663
pratikshanaik12 <41194108+pratikshanaik12@users.noreply.github.com>
2024-03-26 22:35:51 -0400

    Use Effect

---
+++ 11tut/src/App.js
@@ -0,0 +1,79 @@
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import { useState, useEffect } from 'react';
+
+import AddItem from './AddItem';
+import SearchItem from './SearchItem';
+
+function App() {
+
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+
+  const [newItem, setNewItem] = useState('')
+
+  const [search, setSearch] = useState('')
+
+  useEffect(()=>{
+    localStorage.setItem('shoppinglist', JSON.stringify(items))
+  },[items])
+
+const addItem = (item) => {
+  const id = items.length ? items[items.length - 1].id + 1 : 1
+  const myNewItem = {id, checked: false, item}
+  const listItems = [...items, myNewItem]
+  setItems(listItems)
+}
+
+const handleCheck = (id) => {
+  const listItems = items.map((item)=> item.id === id ? 
+  {...item, checked : !item.checked}
+  : item)
+  setItems(listItems)
+}
+
+const handleDelete = (id) => {
+  const listItems = items.filter((item) => item.id !== id )
+  setItems(listItems)
+}
+
+const handleSubmit = (e) => {
+
+  // stops auto reload and maintans state
+  e.preventDefault();
+  if (!newItem) return;
+  // console.log(newItem)
+
+  // addItem
+  addItem (newItem);
+  setNewItem('')  // sets form to empty after pressing enter, so that next thing can be typed
+  
+}
+  
+  return (
+    <div className="App">
+      <Header title = "Grocery List" />
+      
+      <AddItem
+        newItem = {newItem}
+        setNewItem = {setNewItem}
+        handleSubmit = {handleSubmit}
+      />
+
+      <SearchItem
+        search={search}
+        setSearch = {setSearch}
+      />
+
+      <Content
+        items = {items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        handleCheck = {handleCheck}
+        handleDelete = {handleDelete}
+      />
+      
+      <Footer length = {items.length} />
+    </div>
+  );
+}
+
+export default App;
